211b579826baa922c9fabeebb22110d706a48892
Edward Paget <edward@cassetteta.pe>
2015-12-06 08:58:37 -0600

    Working text replacement functions

--- parinfer-mode.el
+++ parinfer-mode.el
@@ -1,3 +1,5 @@
+;;; -*- lexical-binding: t -*-
+
 (defvar proc
   (start-process-shell-command "parinfer" "*parinfer-process*" "./bin/parinfer-mode"))
 
@@ -5,10 +7,15 @@
   (let ((url-request-method "POST")
         (url-request-extra-headers '(("Content-Type" . "text/plain")))
         (url-request-data text))
-    (url-retrieve url 'parinfer-mode-kill-and-replace-buffer))
-  )
+    (url-retrieve url (parinfer-mode-kill-and-replace-buffer))))
 
-(defun parinfer-mode-kill-and-replace-buffer (status)
-  (switch-to-buffer (current-buffer)))
+(defun parinfer-mode-kill-and-replace-buffer ()
+  (let ((old-buffer (current-buffer)))
+    (lambda (status)
+      ;;      (copy-to-buffer old-buffer 1 (buffer-size))
+      (re-search-forward "^$" nil 'move)
+      (forward-char)
+      (copy-to-buffer old-buffer (point) (buffer-size))
+      (kill-buffer (current-buffer)))))
 
 (parinfer-mode-post "http://localhost:8088/indent-mode" (buffer-string))
